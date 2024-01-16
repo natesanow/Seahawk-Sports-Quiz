0bb571966f68c6ef35f728c0ce86430b2eabec0e
natesanow <152323081+natesanow@users.noreply.github.com>
2024-01-15 16:34:25 -0800

    added series of multiple choice questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var highScoresContainerEl = document.querySelector("#high-scores-container");
 var scoresContainer = document.querySelector("#high-scores");
 
 var currentQuestionIndex = 0;
-var timeRemaining = 60;
+var timeRemaining = 75;
 
 var scoresArray;
 if (localStorage.getItem("scores")) {
@@ -27,4 +27,34 @@ localStorage.setItem("scores", JSON.stringify(scoresArray));
 var data = JSON.parse(localStorage.getItem("scores"));
 
 var viewHighScoresLinkEl = document.querySelector("#view-high-scores");
+
+var questionArray = [
+    {
+        question: "What year did the Seahawks win the Super Bowl?",
+        answers: ["2006", "2012", "2014", "2018"],
+        correct: "2014",
+    },
+    {
+        question: "Who won Super Bowl MVP in their Super Bowl win?",
+        answers: ["Russell Wilson", "Richard Sherman", "Malcolm Smith", "Marshawn Lynch"],
+        correct: "Malcolm Smith",
+    },
+    {
+        question: "Who has the most Receptions in Seahawks history?",
+        answers: ["Tyler Lockett", "Doug Baldwin", "Darrell Jackson", "Steve Largent"],
+        correct: "Steve Largent",
+    },
+    {
+        question: "Which Seahawks running back was famously known as Beast Mode?",
+        answers: ["Shaun Alexander", "Marshawn Lynch", "Ricky Watters", "Chris Warren"],
+        correct: "Marshawn Lynch",
+    },
+    {
+        question: "Who has the most touchdowns in Seahawks history?",
+        answers: ["Shaun Alexander", "Steve Largent", "Marshawn Lynch", "Tyler Lockett"],
+        correct: "Shaun Alexander"
+    },
+];
+
+
 
