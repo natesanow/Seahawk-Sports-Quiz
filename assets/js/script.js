e9cfa4e18e13843d7345812c21ca401f509802da
natesanow <152323081+natesanow@users.noreply.github.com>
2024-01-15 16:01:34 -0800

    added time variable and high score variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,18 @@ var formEl = document.querySelector("#form");
 var restartButton = document.querySelector("#btn-restart");
 var highScoresContainerEl = document.querySelector("#high-scores-container");
 var scoresContainer = document.querySelector("#high-scores");
+
+var currentQuestionIndex = 0;
+var timeRemaining = 60;
+
+var scoresArray;
+if (localStorage.getItem("scores")) {
+    scoresArray = JSON.parse(localStorage.getItem("scores"));
+} else {
+    scoresArray = [];
+}
+localStorage.setItem("scores", JSON.stringify(scoresArray));
+var data = JSON.parse(localStorage.getItem("scores"));
+
+var viewHighScoresLinkEl = document.querySelector("#view-high-scores");
+
